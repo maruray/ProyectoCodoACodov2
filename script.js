3febea629e46da87230cd34f9ee86593dadbac22
maruray <111885306+maruray@users.noreply.github.com>
2022-10-14 00:27:23 -0300

    comit3

--- script.js
+++ script.js
@@ -35,3 +35,61 @@ videos.forEach((video) => {
         onLeaveBack: () => video.pause(),
     });
 });
+var formulario = document.getElementsByName('forms')[0];  
+
+// --------------------------------------------------------
+// Validar Nombre
+// --------------------------------------------------------
+var validarNombre = function (e) {
+    if (formulario.nombre.value == 0) {     
+        alert("Complete el campo nombre");
+        e.preventDefault();
+    }
+};
+
+// --------------------------------------------------------
+// Validamos Apellido
+// --------------------------------------------------------
+var validarApellido = function (e) {
+    if (formulario.apellido.value == 0) {      
+        alert("Completa el campo apellido");
+        e.preventDefault();
+    }
+};
+
+// --------------------------------------------------------
+// Validar Interes
+// --------------------------------------------------------
+var validarRadio = function (e) {
+    if (formulario.so[0].checked == true ||
+        formulario.so[1].checked == true ||
+        formulario.so[2].checked == true) {
+    } else {  
+        alert("Selecciona un interés");
+        e.preventDefault();
+    }
+};
+
+// --------------------------------------------------------
+// Validamos Terminos y Condiciones
+// --------------------------------------------------------
+var validarCheckbox = function (e) {
+    if (formulario.terminos.checked == false) {
+        alert("Acepta los términos y condiciones");
+        e.preventDefault();
+    }
+};
+
+// --------------------------------------------------------
+// Se ejecuta al presionar submit e invoca a las tres validaciones
+// --------------------------------------------------------
+var validar = function (e) {  
+    validarNombre(e);
+    validarRadio(e);
+    validarCheckbox(e);
+};
+
+// --------------------------------------------------------
+// Espera que se presione "enviar" y llama a "validar"
+// submit es un evento DEL FORM, no del botón!
+formulario.addEventListener("submit", validar);
